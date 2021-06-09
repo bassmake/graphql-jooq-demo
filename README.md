6bbe001dc850f580a82ef25bdfd177f7d43ea1cf
Miroslav Matejovsky <miroslav.matejovsky@gmail.com>
2021-06-09 04:42:30 +0200

    added spotless

---
+++ README.md
@@ -0,0 +1,8 @@
+# GraphQL with jOOQ
+
+## Technologies
+
+### Spotless with ktlint
+
+- [Spotless](https://github.com/diffplug/spotless/tree/main/plugin-gradle)
+- [ktlint](https://ktlint.github.io/)
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
     kotlin("jvm") version "1.5.10"
+    id("com.diffplug.spotless") version "5.12.5"
 }
 
-group = "org.example"
+group = "sk.bsmk"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -11,4 +12,13 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+}
+
+spotless {
+    kotlin {
+        ktlint()
+    }
+    kotlinGradle {
+        ktlint()
+    }
 }
